2c0ba908e700982fbadd7a5d08655cb9ee58a4fd
fhekwn549 <fhekwn549@naver.com>
2026-03-10 12:57:21 +0900

    fix(slam): also constrain real-time scan matcher angular search window

--- src/ugv_else/cartographer/config/localization_2d.lua
+++ src/ugv_else/cartographer/config/localization_2d.lua
@@ -5,7 +5,11 @@ TRAJECTORY_BUILDER.pure_localization_trimmer = {
 }
 POSE_GRAPH.optimize_every_n_nodes = 20
 
--- Limit constraint search window to prevent 180-degree flip
+-- Limit real-time scan matching search window to prevent 180-degree flip
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(20.)
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.5
+
+-- Limit constraint (loop closure) search window
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(30.)
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 3.
 POSE_GRAPH.constraint_builder.min_score = 0.7
